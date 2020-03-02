00dd9e2982daef6441fddee8d9f2e4698761de7a
Yusuf Aykaç <yusuf_aykac@hotmail.com>
2020-03-02 15:35:59 +0300

    fx

--- AspCoreDataTable.Test/wwwroot/js/entity.js
+++ AspCoreDataTable.Test/wwwroot/js/entity.js
@@ -1,22 +1,33 @@
 ﻿var Entity =
 {
-    Init: function (entity, formid, tableid, submitClass = ".entitysubmit", successTitle = null, successMessage = null, errorTitle = null, errorMessage = null) {
-
-        $("body").delegate(submitClass, "click", function (e) {
-            e.preventDefault();
-            var $btn = $(this);
-            Entity.AddOrEdit($btn, entity, formid, tableid, successTitle, successMessage, errorTitle, errorMessage);
-        });
+    Init: function(entity,
+        formid,
+        tableid,
+        submitClass = ".entitysubmit",
+        successTitle = null,
+        errorMessage = null) {
+        $("body").delegate(submitClass,
+            "click",
+            function(e) {
+                e.preventDefault();
+                var $btn = $(this);
+                Entity.AddOrEdit($btn, entity, formid, tableid, successTitle,  errorMessage);
+            });
 
         var entitydt = DataTableFunc.initDataTable(tableid);
     },
-    Validate: function (formid) {
+    Validate: function(formid) {
         if (!$("#" + formid).validationEngine('validate')) {
             return false;
         }
         return true;
     },
-    AddOrEdit: function (btnRef,entity, formid, tableid = null, successTitle = null, successMessage = null, errorTitle = null, errorMessage = null) {
+    AddOrEdit: function(btnRef,
+        entity,
+        formid,
+        tableid = null,
+        successTitle = null,
+        errorMessage = null) {
         if (this.Validate(formid)) {
             var formData = new FormData($('#' + formid)[0]);
             $.ajax({
@@ -27,22 +38,20 @@
                 datatype: 'json',
                 cache: false,
                 processData: false,
-                success: function (data) {
+                success: function(data) {
                     var res = JSON.parse(data);
-                    if (res.Result == 1) {
-                        if (successTitle != null && successMessage != null) {
+                    if (res.Result === 1) {
+                        if (successTitle !== null) {
                             var table = $('#' + tableid);
                             var dt = table.DataTable();
                             dt.ajax.reload();
-                            Alert.showAlert("success", successTitle, successMessage, AlertTypeEnum.Sweet);
-                            Modal.CloseModal(btnRef); 
-                        }
-                        else {
+                            Alert.showAlert("success", successTitle, res.ResultText, AlertTypeEnum.Sweet);
+                            Modal.CloseModal(btnRef);
+                        } else {
                             window.location.href = "/" + entity + "/Index";
                         }
-                    }
-                    else {
-                        if (tableid != null && errorMessage != null && errorTitle != null) {
+                    } else {
+                        if (tableid !== null) {
                             Alert.showAlert("error", errorTitle, errorMessage, AlertTypeEnum.Sweet);
                             Modal.CloseModal(btnRef);
                         }
@@ -51,14 +60,14 @@
             });
         }
     }
-}
+};
 var Modal =
 {
-    CloseModal: function (btn) {
+    CloseModal: function(btn) {
         var $modal = btn.closest('div.custommodal');
         if ($modal) {
             $modal.removeClass("in");
             $modal.modal('hide');
         }
     }
-}
+};
