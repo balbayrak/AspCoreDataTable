64b6c9d8174d657124b67813a3859949f4d53dde
balbayrak <balbayrak87@gmail.com>
2020-03-08 23:56:56 +0300

    general.js commit

--- AspCoreDataTable.Test/wwwroot/js/general.js
+++ AspCoreDataTable.Test/wwwroot/js/general.js
@@ -3,6 +3,7 @@ function BlockInfo(isenabled, target) {
     this.isenabled = isenabled;
     this.target = target;
 }
+
 var SubmitFunc =
 {
     submitClick: function () {
@@ -202,22 +203,12 @@ var BlockFunc =
 
     showSpinnerBlock: function (target) {
         if (target === "#" || target === '' || target === null || typeof target === 'undefined') {
-            //App.blockUI({
-            //    zIndex: 20000,
-            //    iconOnly: true
-            //});
-
             Loader.loaderStart();
         }
         else {
             if (!target.startsWith("#")) {
                 target = '#' + target.trim();
             }
-            //App.blockUI({
-            //    iconOnly: true,
-            //    target: target,
-            //    boxed: false
-            //});
 
             Loader.loaderStart();
         }
@@ -236,6 +227,71 @@ var BlockFunc =
         }
     }
 };
+
+var Loader = {
+    loaderStart: function (data) {
+        $(".big-loader").show();
+        $.blockUI();
+    },
+    loaderFinish: function () {
+        $(".big-loader").hide();
+
+        $.unblockUI();
+    },
+    AjaxTemplate:
+        '<p class="padding-20"><i class="fa fa-spinner fa-spin"></i> İçerik yüklenirken lütfen bekleyiniz...</p>',
+    AjaxSaveTemplate: '<i class="fa fa-spinner fa-spin"></i> İçerik kaydedilirken lütfen bekleyiniz...'
+
+};
+
+
+$(window).on('beforeunload', function () {
+    Loader.loaderStart();
+});
+
+function StartIt() {
+    $(window).on('beforeunload', function () {
+        Loader.loaderStart();
+    });
+    return true;
+}
+
+function StopIt() {
+    $(window).off('beforeunload');
+    Loader.loaderFinish();
+    return true;
+}
+
+$(document).ajaxStart(function (e) {
+    Loader.loaderStart();
+});
+
+$(document).ajaxStop(function (e) {
+    Loader.loaderFinish();
+
+});
+
+$(document).ajaxError(function () {
+    toastr["error"]("Sistemde bir hata oluştu! Daha sonra tekrar deneyiniz.");
+    var $modal = $('div.custommodal');
+    $modal.removeClass("in");
+    $modal.modal('hide');
+});
+
+Number.prototype.format = function (n, x, s, c) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+        num = this.toFixed(Math.max(0, ~~n));
+
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
+
+
+$(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+        Loader.loaderFinish();
+        $.unblockUI();
+    }
+});
 
 $.fn.extend(
     {
@@ -297,6 +353,8 @@ $.extend({
         return true;
     }
 });
+
+
 
 $(document).ready(function () {
     DismissFunc.dismissClick();
